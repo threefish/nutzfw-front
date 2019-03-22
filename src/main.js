c1cb54a4fd19fc6ff661a0628e926b1aa102e9ba
threefish <huchuc@vip.qq.com>
2019-03-22 18:32:32 +0800

    搭建基础平台

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import Vue from "vue";
-import App from "./App.vue";
+import './core/components_use'
 import router from "./router";
 import store from "./store";
+import App from "./App.vue";
 
 Vue.config.productionTip = false;
-
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount("#app");
--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -1,18 +1,100 @@
 <template>
-    <div>
-        login2
+    <div class="main">
+        <a-form id="formLogin" class="user-layout-login">
+            <a-tabs :activeKey="customActiveKey" :tabBarStyle="{ textAlign: 'center', borderBottom: 'unset' }"
+                    @change="handleTabClick">
+                <a-tab-pane key="tab1" tab="账号密码登陆">
+                    <a-form-item>
+                        <a-input size="large" type="text" placeholder="帐户名/ admin" @change="handleCheckCaptcha"
+                                 v-model.trim="userName">
+                            <a-icon slot="prefix" type="user" :style="{ color: 'rgba(0,0,0,.25)' }"/>
+                        </a-input>
+                    </a-form-item>
+                    <a-form-item>
+                        <a-input size="large" type="password" autocomplete="false" placeholder="密码 / admin"
+                                 v-model="userPass">
+                            <a-icon slot="prefix" type="lock" :style="{ color: 'rgba(0,0,0,.25)' }"/>
+                        </a-input>
+                    </a-form-item>
+                    <a-form-item v-if="showCaptcha">
+                        <a-input size="large" v-model="captcha" type="text" placeholder="验证码">
+                            <a-icon slot="prefix" type="mail" :style="{ color: 'rgba(0,0,0,.25)' }"/>
+                        </a-input>
+                        <a-icon slot="prefix" type="lock" :style="{ color: 'rgba(0,0,0,.25)' }"/>
+                    </a-form-item>
+                    <a-form-item>
+                        <a-checkbox @change="rememberMeChange">自动登陆</a-checkbox>
+                    </a-form-item>
+                    <a-form-item style="margin-top:24px">
+                        <a-button size="large" type="primary" class="login-button" :loading="loginBtn"
+                                  :disabled="loginBtn" @click="handleSubmit">确定
+                        </a-button>
+                    </a-form-item>
+                </a-tab-pane>
+                <a-tab-pane key="tab2" tab="扫码登录">
+                    213
+                </a-tab-pane>
+            </a-tabs>
+        </a-form>
         <router-view/>
     </div>
 </template>
 <script>
-    import {notification} from 'ant-design-vue'
+    import {timeFix} from '@/utils/util'
 
     export default {
         data() {
-            return {}
+            return {
+                customActiveKey: 'tab1',
+                loginBtn: false,
+                showCaptcha: false,
+                rememberMe: false,
+                userName: '',
+                userPass: '',
+                captcha: '',
+            }
         },
         mounted() {
-            notification.info({message: '系统提示', description: "呵呵"})
+            this.$notification.info({message: '系统提示', description: "呵呵"})
+        },
+        methods: {
+            rememberMeChange(e) {
+                this.rememberMe = e.target.checked;
+            },
+            handleCheckCaptcha() {
+                const loginParams = {
+                    username: this.userName,
+                    password: this.userPass
+                }
+                console.log(loginParams)
+            },
+            handleTabClick(key) {
+                this.customActiveKey = key
+            },
+            handleSubmit() {
+                const loginParams = {
+                    username: this.userName,
+                    password: this.userPass,
+                    captcha: this.captcha,
+                }
+                console.log(loginParams)
+            },
+            loginSuccess(res) {
+                console.log(res)
+                this.$router.push({name: 'dashboard'})
+                // 延迟 1 秒显示欢迎信息
+                this.$notification.success({
+                    message: '欢迎',
+                    description: `${timeFix()}，欢迎回来`
+                })
+            },
+            requestFailed(err) {
+                this.$notification['error']({
+                    message: '错误',
+                    description: ((err.response || {}).data || {}).message || '请求出现错误，请稍后再试',
+                    duration: 4
+                })
+            }
         }
     }
 </script>
